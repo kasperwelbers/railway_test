3be4b098112279475120ec2ccbc0bb12518f89d0
Kasper Welbers <kasperwelbers@gmail.com>
2024-05-01 14:31:23 +0200

    abs path

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 
 
-with open(r"data/results.csv", "a") as f:
+with open(r"/data/results.csv", "a") as f:
     w = csv.writer(f)
     date = datetime.now()
     w.writerow([date])
